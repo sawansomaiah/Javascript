882c6f274729df8cfc6f8286f6d784f84ff4b45a
Sawan Somaiah <somaiah.rocks@gmail.com>
2023-02-02 10:50:46 +0530

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -12,13 +12,83 @@
 // createBooking('LH123',undefined , 3)
 
 
-const greet = greeting => {
-        return name => {
-        console.log(`${greeting} ${name}`)
-}
+// const greet = greeting => {
+//         return name => {
+//         console.log(`${greeting} ${name}`)
+// }
+// }
+
+// const greeterHey = greet('Hey')
+// greeterHey('Jonas')
+
+// greet('Hey')('Jonas')
+
+// const luftansa = {
+//         airline: 'Luftansa',
+//         iatcode: 'LH',
+//         bookings: [],
+//         book(flightNum, name){
+//                 console.log(`${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightNum}`);
+//                 this.bookings.push({flight : `${this.iatcode}${flightNum}`, name})
+//                 console.log(this.bookings)
+//         }      
+// }
+
+// luftansa.book(239, 'Sawan')
+// luftansa.book(239, 'Sawan')
+// console.log(luftansa)
+
+
+// Bind method to event listener function
+
+// luftansa.planes = 300;
+// luftansa.buyPlane = function(){
+//         console.log(this);
+//         this.planes++;
+//         console.log(luftansa.planes);
+// }
+
+// document.querySelector('.buy').addEventListener('click', luftansa.buyPlane.call(luftansa))
+
+// const addTax = value =>{
+//         return rate => {
+//                 console.log(`value : ${value + value * rate}`)
+// }
+// }
+
+// addTax(100)(0.23)
+
+
+//Challenge #1
+
+const poll = { 
+        question: 'What is your favourite programming language?',
+        options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+        // This generates [0, 0, 0, 0]. More in the next section 😃
+        answers: new Array(4).fill(0),
+
+        registerNewAnswer : function(){
+        const answer =  Number(prompt(`${this.question}\n${this.options[0]}\n${this.options[1]}\n${this.options[2]}\n${this.options[3]}\n(Write option number)`))
+        if(answer <= 3){
+                if(answer === 0){this.answers[0]++}
+                else if(answer === 1){this.answers[1]++}
+                else if(answer === 2){this.answers[2]++}
+                else if(answer === 3){this.answers[3]++}
+                else{alert(`Please input the correct option`)}
+                }
+                this.displayResults();
+        },
+
+        displayResults: function(){
+        console.log(this.answers)
+        },
+
 }
 
-const greeterHey = greet('Hey')
-greeterHey('Jonas')
+document.querySelector('.poll').addEventListener('click',poll.registerNewAnswer.bind(poll));
 
-greet('Hey')('Jonas')
+
+poll.displayResults.call({answers :[1, 5, 3, 9, 6, 1] })
+
+
+
