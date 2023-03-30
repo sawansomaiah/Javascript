bee620884380c6b1be5232ac3500783f12004d3f
Sawan Somaiah <somaiah.rocks@gmail.com>
2023-03-30 13:19:31 +0530

    Map method

--- 11-Arrays-Bankist/final/script.js
+++ 11-Arrays-Bankist/final/script.js
@@ -353,8 +353,7 @@ Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia'
 
 HINT: Use tools from all lectures in this section so far 😉
 
-TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
 
 GOOD LUCK 😀
 */
